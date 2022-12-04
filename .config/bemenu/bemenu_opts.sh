12a19853b9348c910500f8096a3c807fd3f1a700
Alex <alex@ahubbard.xyz>
2022-12-04 10:37:55 -0600

    update ajh-base to 1.4, enable cursor warp in sway, modify bemenu_opts.sh to be dmenu defaults

--- .config/bemenu/bemenu_opts.sh
+++ .config/bemenu/bemenu_opts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #dmenu theme
 #export BEMENU_OPTS="-H 22   --tb #1d2021 --tf #fabd2f --hb #1d2021  --hf #ebdbb2 --sf #ebdbb2  --fb #1d2021 --sb #3c3836 --ff #ffffff --nb #1d2021  " #--nf #ebdbb2
- export BEMENU_OPTS="-H 24 --tb #285577 --hb #285577 --tf #eeeeee --hf #eeeeee --nf #bbbbbb"
+ export BEMENU_OPTS="-H 22 --tb #285577 --hb #285577 --tf #eeeeee --hf #eeeeee --nf #bbbbbb"
