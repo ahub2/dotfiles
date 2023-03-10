6ea52a831f079a14ae7e60789140fc13664c06e6
Alex <alex@ahubbard.xyz>
2023-03-09 22:54:58 -0600

    some changes

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 img () {
-    chafa  "$1" --size="$2"x"$3" | sed 's/#/\n#/g'
+    chafa  "$1" --size="$2"x"$3" --animate=false | sed 's/#/\n#/g'
     #chafa --format=symbols  "$1" --size="$2"x"$3"
 }
 
@@ -9,7 +9,7 @@ W="$(($(tput cols)/2 - 10))"
 #H="$(tput lines)"
 
 fn="$1"
-w="$2"
+w="$(($2 - 5))"
 h="$3"
 x="$4"
 y="$5"
@@ -22,7 +22,7 @@ case "$fn" in
         *.jpg | *.JPG | *.jpeg) img "$fn" "$w" "$h";;
         *.webp) img "$fn" "$w" "$h";;
         *.png | *.svg | *.gif) img "$fn" "$w" "$h";; #viu -t "$1" -w "$W" ;; #chafa "$fn" --size="$w"x"$h" ;;
-        *.mp4 | *.mkv | *.webm | *.avi | *.mpg | *.mpeg | *.ogv | *.flv | *.wmv | *.WMV) ffmpegthumbnailer -i "$fn" -c jpeg -o - | img - "$w" "$h";; 
+        *.mp4 | *.mkv | *.webm | *.avi | *.mpg | *.mpeg | *.ogv | *.flv | *.wmv | *.WMV) ffmpegthumbnailer -s 0 -i "$fn" -c jpeg -o - | img - "$w" "$h";; 
         *.pdf) pdftotext "$1" -;;
         *) bat -f "$1";; #*) highlight -O ansi "$1" || cat "$1";;
 esac
